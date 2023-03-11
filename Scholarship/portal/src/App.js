ddab81a1ac30d6b464d91661da42094e68ae69f8
001kankana <001kankana@gmail.com>
2023-03-11 11:14:12 +0000

    signup page

--- Scholarship/portal/src/App.js
+++ Scholarship/portal/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes,Route } from 'react-router-dom';
 import Home from "./pages/Home"
 import Login from './pages/login';
-
+import SignUp from './pages/signUp';
 function App() {
   return (
    <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/login' element={<Login/>}/>
+    <Route path='/register' element={<SignUp/>}/>
+
 
    </Routes> 
   );
