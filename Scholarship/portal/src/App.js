e823731e71855f58907f23c7d9e4e5781ba855d0
Kankana Majumder <001kankana@gmail.com>
2023-03-11 23:41:13 +0530

    update

--- Scholarship/portal/src/App.js
+++ Scholarship/portal/src/App.js
@@ -3,13 +3,14 @@ import { Routes,Route } from 'react-router-dom';
 import Home from "./pages/Home"
 import Login from './pages/login';
 import SignUp from './pages/signUp';
+import Dashboard from "./pages/dashboard"
 function App() {
   return (
    <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/login' element={<Login/>}/>
     <Route path='/register' element={<SignUp/>}/>
-
+    <Route path='/dashboard' element={<Dashboard/>}/>
 
    </Routes> 
   );
