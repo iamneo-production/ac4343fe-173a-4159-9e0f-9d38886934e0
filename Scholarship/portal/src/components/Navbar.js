44cf20d85665804cc49d8d8f01a16e3110ced5a5
Kankana Majumder <001kankana@gmail.com>
2023-03-11 23:52:34 +0530

    update

--- Scholarship/portal/src/components/Navbar.js
+++ Scholarship/portal/src/components/Navbar.js
@@ -5,9 +5,11 @@ export default function Navbar() {
   return (
     <div>
        <nav className='navbar'>
+        <Link to="/">
         <div className='logo mx-3' >
           ScholarShips
           </div>
+          </Link>
           <div className='navBtn'>
             
             <Link className='signbtn inBtn' to="/login">
